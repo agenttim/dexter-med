13b074c4ada1213e10d8c5dbe6c48bedee39b6e3
agent_tim <agent_tim@mail.ru>
2023-11-08 13:19:39 +0500

    добавил нижнюю навигацию

--- src/navigation/AppNavigation.js
+++ src/navigation/AppNavigation.js
@@ -1,23 +1,63 @@
 import React from 'react';
 import {NavigationContainer} from "@react-navigation/native";
-import {createMaterialBottomTabNavigator} from '@react-navigation/material-bottom-tabs';
 import {MainScreen} from "../screens/MainScreen";
 import {DocumentsScreen} from "../screens/DocumentsScreen";
+import {ProfileScreen} from "../screens/ProfileScreen";
+import {createBottomTabNavigator} from "@react-navigation/bottom-tabs";
+import {Feather} from '@expo/vector-icons';
+import {THEME} from "../theme";
 
-const Tab = createMaterialBottomTabNavigator();
+
+const Tab = createBottomTabNavigator();
 
 
 export const AppNavigation = ({}) => {
     return (
         <NavigationContainer>
-            <Tab.Navigator>
+            <Tab.Navigator
+                screenOptions={{
+                    tabBarActiveTintColor: THEME.MAIN_COLOR,
+                    tabBarInactiveTintColor: THEME.INACTIVE_COLOR,
+                    tabBarLabelStyle: {
+                        fontSize: 13
+                    },
+                    tabBarStyle: {
+                        height: 70,
+                        paddingTop: 10,
+                        paddingBottom: 10
+                    }
+                }}
+            >
                 <Tab.Screen
                     name="Main"
                     component={MainScreen}
                     options={{
-
-                    }}/>
-                <Tab.Screen name="Documents" component={DocumentsScreen}/>
+                        tabBarLabel: "Главная",
+                        tabBarIcon: ({color}) => (
+                            <Feather name="home" size={25} color={color}/>
+                        )
+                    }}
+                />
+                <Tab.Screen
+                    name="Documents"
+                    component={DocumentsScreen}
+                    options={{
+                        tabBarLabel: "Медкарта",
+                        tabBarIcon: ({color}) => (
+                            <Feather name="book" size={25} color={color}/>
+                        )
+                    }}
+                />
+                <Tab.Screen
+                    name="Profile"
+                    component={ProfileScreen}
+                    options={{
+                        tabBarLabel: "Профиль",
+                        tabBarIcon: ({color}) => (
+                            <Feather name="user" size={25} color={color}/>
+                        )
+                    }}
+                />
             </Tab.Navigator>
         </NavigationContainer>
     )
---
+++ src/theme.js
@@ -0,0 +1,5 @@
+export const THEME = {
+    MAIN_COLOR: '#8465e2',
+    INACTIVE_COLOR: '#a1a8d8',
+    DANGER_COLOR: '#d81b60'
+}
