db12c854de588981b7be00a4427d07d825c574ae
agent_tim <agent_tim@mail.ru>
2023-11-10 16:30:25 +0500

    перешел на development build

--- src/screens/DocumentCardScreen.js
+++ src/screens/DocumentCardScreen.js
@@ -1,33 +1,14 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
-import * as Print from 'expo-print';
-import { Asset } from 'expo-asset';
+import {View, Text, StyleSheet} from 'react-native';
 
-export const DocumentCardScreen = ({ route }) => {
+export const DocumentCardScreen = ({route}) => {
     const {document} = route.params;
-
-    const openPDF = async () => {
-        try {
-            const asset = Asset.fromModule(document.file);
-            await asset.downloadAsync();
-            console.log(asset)
-
-            await Print.printAsync({
-                uri: asset.localUri,
-            });
-        } catch (error) {
-            console.log(document.file)
-            console.error("Error while opening PDF:", error);
-        }
-    }
-
-    openPDF()
 
     return (
         <View style={styles.screenStyle}>
-            <Text >{document.description}</Text>
-            <Text >{new Date(document.date)
-                .toLocaleDateString('ru', {separator: '.'})}</Text>
+            <Text>
+                {document.file}
+            </Text>
         </View>
     )
 }
