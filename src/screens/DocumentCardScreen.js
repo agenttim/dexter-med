dde54c1ae244cb774cdbc32bd390ea7b1b2575a4
agent_tim <agent_tim@mail.ru>
2023-11-10 10:29:38 +0500

    реализовал открытие pdf файла через expo-print

--- src/screens/DocumentCardScreen.js
+++ src/screens/DocumentCardScreen.js
@@ -1,12 +1,33 @@
 import React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
+import * as Print from 'expo-print';
+import { Asset } from 'expo-asset';
 
-export const DocumentCardScreen = ({route}) => {
+export const DocumentCardScreen = ({ route }) => {
     const {document} = route.params;
+
+    const openPDF = async () => {
+        try {
+            const asset = Asset.fromModule(document.file);
+            await asset.downloadAsync();
+            console.log(asset)
+
+            await Print.printAsync({
+                uri: asset.localUri,
+            });
+        } catch (error) {
+            console.log(document.file)
+            console.error("Error while opening PDF:", error);
+        }
+    }
+
+    openPDF()
 
     return (
         <View style={styles.screenStyle}>
-            <Text>{document.file}</Text>
+            <Text >{document.description}</Text>
+            <Text >{new Date(document.date)
+                .toLocaleDateString('ru', {separator: '.'})}</Text>
         </View>
     )
 }
@@ -15,6 +36,6 @@ const styles = StyleSheet.create({
     screenStyle: {
         flex: 1,
         justifyContent: 'center',
-        alignItems: 'center'
-    }
-})
+        alignItems: 'center',
+    },
+});
