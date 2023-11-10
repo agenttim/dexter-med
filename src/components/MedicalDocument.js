41524aa13721657277b6f1b338042c6b7b175ec7
agent_tim <agent_tim@mail.ru>
2023-11-10 11:36:00 +0500

    устранил баг с визуальным морганием и тенью при нажатии на выбранный документе

--- src/components/MedicalDocument.js
+++ src/components/MedicalDocument.js
@@ -1,19 +1,20 @@
 import React from 'react';
-import {View, Text, StyleSheet, TouchableOpacity} from 'react-native';
+import {View, Text, StyleSheet, TouchableOpacity, TouchableNativeFeedback} from 'react-native';
 import {THEME} from "../theme";
 
 export const MedicalDocument = ({document, onPress}) => {
     return (
-        <TouchableOpacity onPress={onPress}>
-            <View style={styles.componentStyle}>
-                <Text>{document.description}</Text>
-                <Text>
-                    {new Date(document.date)
-                        .toLocaleDateString('ru', {separator: '.'})}
-                </Text>
-            </View>
+        <TouchableOpacity
+            style={styles.componentStyle}
+            onPress={onPress}
+        >
+            <Text>{document.description}</Text>
+            <Text>
+                {new Date(document.date)
+                    .toLocaleDateString('ru', {separator: '.'})}
+            </Text>
+            <Text>Тест</Text>
         </TouchableOpacity>
-
     )
 }
 
@@ -25,6 +26,7 @@ const styles = StyleSheet.create({
         backgroundColor: 'white',
         borderRadius: 20,
         padding: 15,
-        elevation: 2
+        elevation: 3,
+        backfaceVisibility: "visible"
     }
 })
