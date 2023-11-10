d82e0376fe69ff186ea53896da648e0b66940d0b
agent_tim <agent_tim@mail.ru>
2023-11-10 16:57:49 +0500

    открываю pdf по ссылке в web

--- src/screens/DocumentCardScreen.js
+++ src/screens/DocumentCardScreen.js
@@ -1,14 +1,21 @@
 import React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import {View, Text, StyleSheet, Dimensions} from 'react-native';
+import Pdf from "react-native-pdf";
 
 export const DocumentCardScreen = ({route}) => {
     const {document} = route.params;
+    const PdfRead = {uri: 'https://getfile.dokpub.com/yandex/get/https://disk.yandex.ru/i/1F5doIa4LoW8JQ', cache: true}
 
     return (
         <View style={styles.screenStyle}>
-            <Text>
-                {document.file}
-            </Text>
+            <Pdf
+                source={PdfRead}
+                style={styles.pdf}
+                trustAllCerts={false}
+                onLoadComplete={(numberOfPages, filePath) => {
+                    console.log(`number of pages: ${numberOfPages}`);
+                }}
+            />
         </View>
     )
 }
@@ -19,4 +26,9 @@ const styles = StyleSheet.create({
         justifyContent: 'center',
         alignItems: 'center',
     },
+    pdf: {
+        flex: 1,
+        width: Dimensions.get('window').width,
+        height: Dimensions.get('window').height,
+    }
 });
